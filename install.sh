e83604e65ac4eda6d41cbb48b7ec721e4f742236
Kapcioszek <BartGOKU@onet.pl>
2023-04-23 15:17:56 +0200

    changed install script

--- install.sh
+++ install.sh
@@ -2,17 +2,17 @@
 
 mkdir download
 cd download
-curl https://dl.suckless.org/dwm/dwm-6.4.tar.gz -o dwm-6.4.tar.gz
+git clone https://github.com/theKapcioszek/dwm
 curl https://dl.suckless.org/tools/dmenu-5.2.tar.gz -o dmenu-5.2.tar.gz
 git clone https://git.suckless.org/slstatus
 git clone https://github.com/LukeSmithxyz/st
 tar -zxvf dwm-6.4.tar.gz
 tar -zxvf dmenu-5.2.tar.gz
-sudo cp ../configs/dwm/config.h dwm-6.4/
+sudo cp ../configs/dwm/config.h dwm/
 sudo cp ../configs/dmenu/config.h dmenu-5.2/
 sudo cp ../configs/slstatus/config.h slstatus/
 sudo cp ../configs/st/config.h st/
-cd dwm-6.4/
+cd dwm/
 make clean && sudo make install
 cd ..
 cd dmenu-5.2
